95773ea4831ee2f76804ac847c3772ef08bee558
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2022-12-02 17:18:08 +0100

    Create Achat.java

---
+++ app/src/main/java/com/example/realm_demo/models/Achat.java
@@ -0,0 +1,42 @@
+package com.example.realm_demo.models;
+
+import java.util.Date;
+
+import io.realm.RealmList;
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Achat extends RealmObject {
+    @PrimaryKey
+    private long id;
+    private RealmList<Product> products;
+    private Date date;
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public RealmList<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(RealmList<Product> products) {
+        this.products = products;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
